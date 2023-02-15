e357c28ae80e1933197be68ec347fb4d61777ac3
Hebert <hebertLandim007@outlook.com>
2023-02-15 20:44:08 -0300

    Static

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -4,8 +4,11 @@ public class Conta {
     private int agencia; 
     private int numero; 
     private Cliente titular;
+    private static int total;
     
     public Conta( int agencia, int numero) { 
+    	Conta.total++;
+    	System.out.println("O total de contas é " + Conta.total);
         this.agencia = agencia;
         this.numero = numero;
         System.out.println("estou criando uma conta " + this.numero);
@@ -66,5 +69,9 @@ public class Conta {
 	
 	public Cliente getTitular() {
 		return titular;
+	}
+	
+	public static int getTotal() {
+		return total;
 	}
 }
