07eef8924d6ae6d78bccb12bc26649b05f79e778
Hebert <hebertLandim007@outlook.com>
2023-02-28 16:54:42 -0300

    Java does not have multiple inheritance

--- bytebank-herdado/src/SistemaInterno.java
+++ bytebank-herdado/src/SistemaInterno.java
@@ -3,7 +3,7 @@ public class SistemaInterno {
 
 	private int senha = 2222;
 
-	public void autentica(FuncionarioAutenticavel g) {
+	public void autentica(Autenticavel g) {
 		boolean autenticou = g.autentica(this.senha);
 		if(autenticou) {
 			System.out.println("Pode entrar no sistema!");
